d577449153d1eecf4463cffd980927d052ccf8f6
rc420head <dennislebhardt@yahoo.com>
2015-11-16 08:53:20 -0600

    add back HTC_CIR to fix ir blaster

--- hima-common/Android.mk
+++ hima-common/Android.mk
@@ -19,6 +19,16 @@ LOCAL_PATH := $(call my-dir)
 ifneq ($(filter hima himaul himawl himawhl,$(TARGET_DEVICE)),)
 
 include $(CLEAR_VARS)
+LOCAL_MODULE        := HTC_CIR
+LOCAL_MODULE_CLASS  := APPS
+LOCAL_MODULE_OWNER  := htc
+LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
+LOCAL_MODULE_TAGS   := optional
+LOCAL_CERTIFICATE   := platform
+LOCAL_SRC_FILES     := proprietary/priv-app/HTC_CIR/HTC_CIR.apk
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
 LOCAL_MODULE := TimeService
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_OWNER  := htc
